d8357b4ca899f075615fb30222b25ddf0359d9c0
prakhargvp <prakhargvp@gmail.com>
2022-04-04 08:09:03 +0530

    feat: Hello World Project

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Prakhar");
+		
+		int myFirstNumber = (10 * 5) + (2 + 3);
+		int mySecondNumber = 12;
+		int myThirdNumber = 6;
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+		System.out.println(myTotal);
+	}
+}
